cb21b1f846b28b806e984a8fb2d5d0dc4e706b71
puig_j <34269624+TerenceVy@users.noreply.github.com>
2017-11-20 12:54:08 +0000

    ::

---
+++ partials/test_cart2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Description produit</title>
+</head>
+
+<body>
+	DESCRIPTION
+
+<?php 
+$db = new PDO ('mysql:host=localhost;dbname=etnamanga_vy_t', 'root', 'salutlesbro');
+$req = $db->query('SELECT * FROM Produits');
+    $test->execute();
+$results = $test->fetchALL();
+foreach ($results as $key) {
+	?>
+	<table border="20" bgcolor="yellow">
+		<tr>
+		<td>
+		<?php
+	echo $key['ID'];?></td>
+	<td><?php
+	echo $key['Libelle'];?></td>
+	<td><?php
+	echo $key['Description'];?></td>
+	<td><?php
+	echo $key['Prix_vente'];?></td>
+	<td><?php
+	echo $key['Nombres_produit'];?></td>
+	</tr>
+	</table>
+<?php	
+}
+?>
+
+</body>
+</html>
